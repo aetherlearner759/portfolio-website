02aa0168e3f34674e18e4b38d652beb89aea7533
aetherlearner759 <aetherlearner759@gmail.com>
2025-08-08 14:52:06 -0600

    Set up path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig(() => {
 	return {
 		plugins: [react()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+				"@components": path.resolve(__dirname, "./src/components"),
+				"@assets": path.resolve(__dirname, "./src/assets"),
+			},
+		},
 	};
 });
