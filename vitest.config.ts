a531c4bc21dfc825ff766aea636c5a5125abe29c
aetherlearner759 <aetherlearner759@gmail.com>
2025-08-14 10:13:04 -0600

    Modified to more suitable test configuration
    - More accurate naming
    - Path alias
    - Better set up and clean up

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,31 +1,48 @@
 /// <reference types="vitest" />
-import { defineConfig, mergeConfig } from "vitest/config";
+import { defineConfig, configDefaults, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig((configEnv) =>
 	mergeConfig(
 		viteConfig(configEnv),
 		defineConfig({
+			resolve: {
+				alias: {
+					"@test": path.resolve(__dirname, "./src/__test__"),
+					"@test/mocks": path.resolve(__dirname, "./src/__test__/mocks"),
+				},
+			},
 			test: {
 				projects: [
 					{
 						extends: true,
 						test: {
-							name: "Node unit tests",
+							name: "Client tests",
 							globals: true,
-							environment: "node",
+							environment: "jsdom",
 							include: ["**/*.test.ts"],
+							exclude: [...configDefaults.exclude, "**/use*.test.ts"],
+							setupFiles: ["./src/__test__/mockSetUp.ts"],
 						},
 					},
 					{
 						extends: true,
 						test: {
-							name: "jsdom unit tests",
+							name: "React tests",
 							globals: true,
 							environment: "jsdom",
-							include: ["**/*.test.tsx"],
-							setupFiles: ["./src/__test__/setUp.tsx"],
+							include: ["**/*.test.tsx", "**/use*.test.ts"],
+							setupFiles: [
+								"./src/__test__/mockSetUp.ts",
+								"./src/__test__/reactSetUp.ts",
+							],
+							sequence: {
+								// to enforce react clean up first then mock clean up.
+								hooks: "stack",
+								setupFiles: "list",
+							},
 						},
 					},
 				],
