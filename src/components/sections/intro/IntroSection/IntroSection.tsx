245c9627a78390ef2582205db98bb7f466099d07
aetherlearner759 <aetherlearner759@gmail.com>
2025-08-21 20:56:34 -0600

    Implemented IntroSection component

---
+++ src/components/sections/intro/IntroSection/IntroSection.tsx
@@ -0,0 +1,17 @@
+import commonStyle from "../../common.module.css";
+import style from "./IntroSection.module.css";
+import { FaCaretDown } from "react-icons/fa";
+
+export default function IntroSection() {
+	return (
+		<div className={`${commonStyle.root} ${style.root}`}>
+			<h1 className={style.title}>Scott Hur</h1>
+			<p className={style.subtitle}>Aspiring Full-Stack Developer</p>
+
+			<div className={style["scroll-indicator"]}>
+				Scroll down
+				<FaCaretDown />
+			</div>
+		</div>
+	);
+}
